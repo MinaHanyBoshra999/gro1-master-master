37c1d060a846dcdc05ca13fbfed38ac373d9f9cd
sciencecomputer555 <105368893+sciencecomputer555@users.noreply.github.com>
2023-08-14 13:03:39 +0300

    newVersion

--- lib/view/Branch_PAGE.dart
+++ lib/view/Branch_PAGE.dart
@@ -22,102 +22,286 @@ class BranchPage extends StatelessWidget {
       body: Padding(
 
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
+        child: SingleChildScrollView(
+
+          child: Column(
+            children: [
             const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text('اختار الفرع',style: TextStyle(fontFamily: 'Alexandria',color:Colors.blue,fontSize: 22),),
-              ],
-            ),
-            SizedBox(height: 20,),
-            BranchWidget(
-              cref3: FirebaseFirestore.instance.collection('users'),
-              CrefBranchFried: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('FriedItems')
-                  .collection('FriedItemsList') ,
-              CrefBranchRaw:  FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('RawItems')
-                  .collection('RawItemsList'),
-              CrefstreamBranchSauces: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('SauceItems')
-                  .collection('SauceItemsList'),
-              deleteRef: FirebaseFirestore.instance.collection('HosaryOrders'),
-              streamBranch: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .snapshots(),
-              branchName: 'Hosary',
-              cref2: FirebaseFirestore.instance.collection('HosaryShopItems'),
-              name: 'Orders',
-              name2: 'shop',
-              streamBranchOrders: FirebaseFirestore.instance
-                  .collection('HosaryOrders')
-                  .snapshots(),
-              streamBranchRaw: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('RawItems')
-                  .collection('RawItemsList')
-                  .snapshots(),
-              streamBranchFried: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('FriedItems')
-                  .collection('FriedItemsList')
-                  .snapshots(),
-              streamBranchSauces: FirebaseFirestore.instance
-                  .collection('HosaryShopItems')
-                  .doc('SauceItems')
-                  .collection('SauceItemsList')
-                  .snapshots(),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            BranchWidget(
-              cref3: FirebaseFirestore.instance.collection('users'),
-              CrefstreamBranchSauces:FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('SaucesItems')
-                  .collection('SauceItemsList') ,
-              CrefBranchRaw:FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('RawItems')
-                  .collection('RawItemsList') ,
-              CrefBranchFried: FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('FriedItems')
-                  .collection('FriedItemsList') ,
-              deleteRef:FirebaseFirestore.instance.collection('MohandseenOrders') ,
-              streamBranch: FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .snapshots(),
-              branchName: 'Mohandseen',
-              cref2:
-                  FirebaseFirestore.instance.collection('MohandseenShopItems'),
-              name: 'Orders',
-              name2: 'shop',
-              streamBranchOrders: FirebaseFirestore.instance
-                  .collection('MohandseenOrders')
-                  .snapshots(),
-              streamBranchRaw: FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('RawItems')
-                  .collection('RawItemsList')
-                  .snapshots(),
-              streamBranchFried: FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('FriedItems')
-                  .collection('FriedItemsList')
-                  .snapshots(),
-              streamBranchSauces: FirebaseFirestore.instance
-                  .collection('MohandseenShopItems')
-                  .doc('SaucesItems')
-                  .collection('SauceItemsList')
-                  .snapshots(),
-            ),
-          ],
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('اختار الفرع',style: TextStyle(fontFamily: 'Alexandria',color:Colors.blue,fontSize: 22),),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                CrefBranchRaw:  FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList'),
+                CrefstreamBranchSauces: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList'),
+                deleteRef: FirebaseFirestore.instance.collection('FaisalOrders'),
+                streamBranch: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .snapshots(),
+                branchName: 'Faisal',
+                cref2: FirebaseFirestore.instance.collection('FaisalShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('FaisalOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('FaisalShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefstreamBranchSauces:FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList') ,
+                CrefBranchRaw:FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList') ,
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                deleteRef:FirebaseFirestore.instance.collection('HaramOrders') ,
+                streamBranch: FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .snapshots(),
+                branchName: 'Haram',
+                cref2:
+                    FirebaseFirestore.instance.collection('HaramShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('HaramOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('HaramShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+              const SizedBox(
+          height: 10,
+      ),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefstreamBranchSauces:FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList') ,
+                CrefBranchRaw:FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList') ,
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                deleteRef:FirebaseFirestore.instance.collection('OctoberOrders') ,
+                streamBranch: FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .snapshots(),
+                branchName: 'October',
+                cref2:
+                FirebaseFirestore.instance.collection('OctoberShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('OctoberOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('OctoberShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefstreamBranchSauces:FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList') ,
+                CrefBranchRaw:FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList') ,
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                deleteRef:FirebaseFirestore.instance.collection('NasrCityOrders') ,
+                streamBranch: FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .snapshots(),
+                branchName: 'NasrCity',
+                cref2:
+                FirebaseFirestore.instance.collection('NasrCityShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('NasrCityOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('NasrCityShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefstreamBranchSauces:FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList') ,
+                CrefBranchRaw:FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList') ,
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                deleteRef:FirebaseFirestore.instance.collection('ZayedCityOrders') ,
+                streamBranch: FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .snapshots(),
+                branchName: 'ZayedCity',
+                cref2:
+                FirebaseFirestore.instance.collection('ZayedCityShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('ZayedCityOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('ZayedCityShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BranchWidget(
+                cref3: FirebaseFirestore.instance.collection('users'),
+                CrefstreamBranchSauces:FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList') ,
+                CrefBranchRaw:FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList') ,
+                CrefBranchFried: FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList') ,
+                deleteRef:FirebaseFirestore.instance.collection('GizaOrders') ,
+                streamBranch: FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .snapshots(),
+                branchName: 'Giza',
+                cref2:
+                FirebaseFirestore.instance.collection('GizaShopItems'),
+                name: 'Orders',
+                name2: 'shop',
+                streamBranchOrders: FirebaseFirestore.instance
+                    .collection('GizaOrders')
+                    .snapshots(),
+                streamBranchRaw: FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('RawItems')
+                    .collection('RawItemsList')
+                    .snapshots(),
+                streamBranchFried: FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('FriedItems')
+                    .collection('FriedItemsList')
+                    .snapshots(),
+                streamBranchSauces: FirebaseFirestore.instance
+                    .collection('GizaShopItems')
+                    .doc('SaucesItems')
+                    .collection('SaucesItemsList')
+                    .snapshots(),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
